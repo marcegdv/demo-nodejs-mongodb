e97db252677bc739fa17557c31f56a9521a896c9
marcegdv <mgdv79@gmail.com>
2021-12-18 15:41:11 -0300

    refactor database.js

--- api/controller/controller.js
+++ api/controller/controller.js
@@ -1,12 +1,12 @@
-import { dbAccess, mdbSetAtlas } from '../database.js';
+import { dbAccess, mdbSetAtlas } from '../services/database.js';
 
-export const getProductos = async (req, res ,next) => {
-    mdbSetAtlas();
+mdbSetAtlas();
+
+export const getProductos = async (req, res) => {
     try {
         const dbResponse = await dbAccess('r', 'productos', {});
         res.send(dbResponse);
     } catch (error) {
         console.log(error);
     };
-    next();
 };
